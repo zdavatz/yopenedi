7ca72a33fd5cf3cbd3ce4c60c46c39713d985a10
b123400 <i@b123400.net>
2020-06-11 22:13:21 +0900

    Use Java 1.8

--- java/openedi-converter/src/main/java/openedi/converter/OpenTrans/Writer.java
+++ java/openedi-converter/src/main/java/openedi/converter/OpenTrans/Writer.java
@@ -2,14 +2,11 @@ package openedi.converter.OpenTrans;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamWriter;
-import java.io.FileWriter;
 import java.io.OutputStream;
 
 public class Writer {
-    public static final String bmecat = "http://www.bmecat.org/bmecat/2005";
-
     public void write(Order order, OutputStream outputStream) throws Exception {
-        XMLOutputFactory xof = XMLOutputFactory.newDefaultFactory();
+        XMLOutputFactory xof = XMLOutputFactory.newFactory();
         XMLStreamWriter xmlWriter = xof.createXMLStreamWriter(outputStream);
         xmlWriter.writeStartDocument();
         order.write(xmlWriter);
