4ddf1c1cd993ee2c6911f7cd7b169369f22eaec1
b123400 <i@b123400.net>
2020-08-15 18:39:33 +0900

    Implement outputting ORDRSP Edifact file

---
+++ java/converter/src/main/java/com/ywesee/java/yopenedi/Edifact/OrderResponseItem.java
@@ -0,0 +1,31 @@
+package com.ywesee.java.yopenedi.Edifact;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class OrderResponseItem {
+    public BigDecimal lineItemId;
+    public String ean;
+    public String supplierSpecificProductId;
+    public String buyerSpecificProductId;
+
+    public String shortDescription;
+    public String longDescription;
+
+    public BigDecimal orderQuantity;
+    public BigDecimal deliveryQuantity;
+
+    public String requestedDeliveryDate;
+    public String promisedDeliveryDate;
+    public String actualDeliveryDate;
+
+    public BigDecimal priceLineAmount;
+    public BigDecimal price;
+    public BigDecimal priceQuantity;
+    public String buyerOrderId;
+    public String buyerOrderItemId;
+    public Date referenceDate;
+
+    public ArrayList<AllowanceOrCharge> allowanceOrCharges;
+}
