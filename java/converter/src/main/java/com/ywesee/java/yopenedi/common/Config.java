5255e445c9f74c503c218e91b705eb6b5e6fb650
b123400 <i@b123400.net>
2020-08-26 22:35:18 +0900

    Add support for overriding gln

--- java/converter/src/main/java/com/ywesee/java/yopenedi/common/Config.java
+++ java/converter/src/main/java/com/ywesee/java/yopenedi/common/Config.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import com.ywesee.java.yopenedi.Edifact.DespatchAdvice;
+import com.ywesee.java.yopenedi.Edifact.Invoice;
+import com.ywesee.java.yopenedi.Edifact.OrderResponse;
+import com.ywesee.java.yopenedi.Edifact.Party;
 import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -69,6 +75,52 @@ public class Config {
         ArrayList<ResultDispatch> dispatches = this.getResultDispatches();
         for (ResultDispatch dispatch : dispatches) {
             dispatch.send(gln, edifactType, file, messageId);
+        }
+    }
+
+    public HashMap<String, String> getGlnOverrideMap() {
+        try {
+            HashMap<String, String> result = new HashMap<>();
+            File f = new File(this.path, "gln-override.json");
+            String str = FileUtils.readFileToString(f, "UTF-8");
+            JSONParser parser = new JSONParser();
+            JSONObject obj = (JSONObject) parser.parse(str);
+            Set<String> keys = obj.keySet();
+            for (String key : keys) {
+                result.put(key, (String)obj.get(key));
+            }
+            return result;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return new HashMap<String, String>();
+    }
+
+    public void replaceGLN(DespatchAdvice despatchAdvice) {
+        for (Party p : despatchAdvice.parties) {
+            replaceGLN(p);
+        }
+    }
+
+    public void replaceGLN(Invoice invoice) {
+        for (Party p : invoice.parties) {
+            replaceGLN(p);
+        }
+    }
+
+    public void replaceGLN(OrderResponse orderResponse) {
+        for (Party p : orderResponse.parties) {
+            replaceGLN(p);
+        }
+    }
+
+    public void replaceGLN(Party party){
+        String name = udxChannel().get(party.id);
+        if (name != null) {
+            String replaced = getGlnOverrideMap().get(name);
+            if (replaced != null) {
+                party.id = replaced;
+            }
         }
     }
 }
