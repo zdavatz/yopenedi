27e796002156de572d5df78b50042356ddbeedd5
b123400 <i@b123400.net>
2021-06-02 22:13:14 +0900

    Duplicate amount for MOA125 #207

--- java/converter/src/main/java/com/ywesee/java/yopenedi/Edifact/Invoice.java
+++ java/converter/src/main/java/com/ywesee/java/yopenedi/Edifact/Invoice.java
@@ -803,6 +803,17 @@ public class Invoice implements Writable, MessageExchange<Party> {
             sg48.setMOAMonetaryAmount(moa);
             sg48s.add(sg48);
         }
+        if (this.netAmountOfItems != null) {
+            SegmentGroup48 sg48 = new SegmentGroup48();
+            MOAMonetaryAmount moa = new MOAMonetaryAmount();
+            segmentCount++;
+            C516MonetaryAmount c516 = new C516MonetaryAmount();
+            c516.setE5025MonetaryAmountTypeQualifier("125");
+            c516.setE5004MonetaryAmount(this.netAmountOfItems);
+            moa.setC516MonetaryAmount(c516);
+            sg48.setMOAMonetaryAmount(moa);
+            sg48s.add(sg48);
+        }
         if (this.taxAmount != null) {
             SegmentGroup48 sg48 = new SegmentGroup48();
             MOAMonetaryAmount moa = new MOAMonetaryAmount();
